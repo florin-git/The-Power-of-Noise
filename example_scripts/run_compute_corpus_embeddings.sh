0638f60e65ea01a6a3cff23b1c218e809ca0a227
Florin <81188062+florin-git@users.noreply.github.com>
2024-08-13 09:51:55 +0300

    Update run_compute_corpus_embeddings.sh

--- example_scripts/run_compute_corpus_embeddings.sh
+++ example_scripts/run_compute_corpus_embeddings.sh
@@ -3,7 +3,7 @@
 # cd ..
 
 python src/compute_corpus_embeddings.py \
-    --corpus_path data/corpus/wiki_dec_2018.json \
+    --corpus_path data/corpus.json \
     --encoder_id facebook/contriever \
     --max_length_encoder 512 \
     --output_dir data/corpus/embeddings/wiki_dec_2018 \
